bf31bb8596c1a9d982479ef816163fd5da692bff
Danny Pham <email": null>
2016-07-01 04:00:37 -0700

    fixed other errror

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::API
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :omniauthable
   include DeviseTokenAuth::Concerns::SetUserByToken
 end
