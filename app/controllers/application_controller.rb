97ff43413ca4857c71aa1a941467e9c2c921429b
Danny Pham <email": null>
2016-07-01 03:58:05 -0700

    fixed error on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
+  protect_from_forgery with: :null_session
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :omniauthable
   include DeviseTokenAuth::Concerns::SetUserByToken
-
 end
