a59dca9fcd654fb7fc2a5b9d05a6311497bb972c
Danny Pham <email": null>
2016-07-01 03:44:19 -0700

    removed protect from forgery from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::API
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :omniauthable
   include DeviseTokenAuth::Concerns::SetUserByToken
 
 end
