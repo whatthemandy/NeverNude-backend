109ad2618077a8a8861335e2a4312827444d3b4f
Mandy McDorman <email": null>
2016-07-03 14:13:44 -0700

    Add new items (photos) to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Create users
-User.create(first_name: 'testing', last_name: 'tester', email: 'test@test.com', password: 'password')
-User.create(first_name: 'danny', last_name: 'pham', email: 'dannynpham@csu.fullerton.edu', password: 'password')
+test_user = User.create(first_name: 'testing', last_name: 'tester', email: 'test@test.com', password: 'password')
+danny = User.create(first_name: 'danny', last_name: 'pham', email: 'dannynpham@csu.fullerton.edu', password: 'password')
 
 # Create sections
 accessory = Section.create(name: "Accessory")
 top = Section.create(name: "Top")
 bottom = Section.create(name: "Bottom")
 feet = Section.create(name: "Feet")
+
+# Create items
+hat = Item.create(user: test_user, section: accessory, image: File.open('/images/item-hat.jpg'))
+shirt = Item.create(user: test_user, section: top, image: File.open('/images/item-shirt.jpg'))
+shorts = Item.create(user: test_user, section: bottom, image: File.open('/images/item-shorts.jpg'))
+shoes = Item.create(user: test_user, section: feet, image: File.open('/images/item-shoes.jpg'))
+watch = Item.create(user: test_user, section: accessory, image: File.open('/images/item-watch.jpg'))
+dress = Item.create(user: test_user, section: top, image: File.open('/images/item-dress.jpg'))
+leggings = Item.create(user: test_user, section: bottom, image: File.open('/images/item-leggings.jpg'))
+heels = Item.create(user: test_user, section: feet, image: File.open('/images/item-shoes.jpg'))
