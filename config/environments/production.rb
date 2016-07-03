c1a7f6e0b8a4a0553902c81f4994d5c407a79e35
Danny Pham <email": null>
2016-07-02 17:26:14 -0700

    Fix aws credential issue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,13 +18,14 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   # config/environments/production.rb
+
+  # Amazon Web Services S3
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
 
