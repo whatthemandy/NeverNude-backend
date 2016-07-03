e8f1e328d3d847c9de5cdc81f072df2f84a1a71b
Danny Pham <email": null>
2016-07-03 03:18:57 -0700

    Still troubleshooting images

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   def index
     @section = Section.find(params[:section_id])
     @items = @section.items
-    response.headers['image_path'] = "#{@items[0].image.url}"
+    response.headers['image_path'] = "#{@items[0].image.url(:medium)}"
     render json: @items
   end
 
