bc213d03dad536a94d531add36d1daefffa7a677
Danny Pham <email": null>
2016-07-02 22:06:56 -0700

    fixed posting to register

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   scope :v1 do
-    mount_devise_token_auth_for 'User', at: 'auth'
+    mount_devise_token_auth_for 'User', at: 'auth', :defaults => { :format => 'json' }
+    match '/auth/register' => 'registrations#create', :via => 'post', :defaults => { :format => 'json' }
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'secret' => 'secret#secret' #secret path
 
