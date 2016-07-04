a4e9ce87b59342ba0b42ddd4afd1fa1f2e1d7c96
Mandy McDorman <email": null>
2016-07-03 17:04:05 -0700

    Update image sizes

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,10 @@ class Item < ApplicationRecord
   belongs_to :section
   has_attached_file :image,
     :styles => {
-      :thumb => "100x100#",
-      :small  => "150x150>",
-      :medium => "300x300",
-      :large => "600x600" },
+      :thumb => "100x75#",
+      :small  => "200x150>",
+      :medium => "300x225",
+      :large => "400x300" },
     storage: :s3,
     s3_credentials: {access_key_id: ENV["AWS_KEY"], secret_access_key: ENV["AWS_SECRET"]},
     bucket: "neverevernude"
