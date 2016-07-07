9249299dd21b1ecd37e20478e78a690154a2d95c
Danny Pham <email": null>
2016-07-07 02:08:53 -0700

    did stuff

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = Item.build(item_params)
     if @item.save
       render json: @item
     else
