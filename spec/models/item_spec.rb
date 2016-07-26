44e234fc905b8464f734ef5b94f43f0f0c526736
whatthemandy <amcdorman@gmail.com>
2016-07-25 19:31:13 -0700

    Update tests to use should-association matchers

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,27 +1,26 @@
-require_relative '../../app/models/item.rb'
+require 'spec_helper'
 
-RSpec.describe Item do
-  # it { expect(subject).to validate_presence_of(:user_id) }
-  # it { expect(subject).to validate_presence_of(:section_id).only_integer }
-
-  # it { expect(subject).to have_many(:outfits_items) }
-  # it { expect(subject).to have_many(:outfits) }
-  # it { expect(subject).to have_many(:tags_items) }
-  # it { expect(subject).to have_many(:tags) }
+describe Item do
+  it { should have_many(:outfits_items) }
+  it { should have_many(:outfits) }
+  it { should have_many(:tags_items) }
+  it { should have_many(:tags) }
+  it { should belong_to(:user) }
+  it { should belong_to(:section) }
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:section_id) }
 
   # it { should have_attached_file(:image) }
   # it { should validate_attachment_presence(:image) }
-
 
   # it { should validate_attachment_content_type(:icon).
   #     allowing('image/png', 'image/gif').
   #     rejecting('text/plain', 'text/xml') }
 
-
   # it { should validate_attachment_size(:image).
   #               less_than(2.megabytes) }
   # it { should validate_attachment_size(:icon).
-                # greater_than(1024) }
+  #               greater_than(1024) }
   # it { should validate_attachment_size(:icon).
-                # in(0..100) }
+  #               in(0..100) }
 end
