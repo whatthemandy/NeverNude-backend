2def5895b2a8b3386a1b2b4cc8b934f36ab3d1a8
whatthemandy <amcdorman@gmail.com>
2016-07-25 19:41:38 -0700

    Test update

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,5 +5,4 @@ describe Tag do
   it { should have_many(:tags_items) }
   it { should have_many(:items) }
   it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
 end
