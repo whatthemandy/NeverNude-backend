359635490b6d8d42a489164c46c185a0ba32649a
whatthemandy <amcdorman@gmail.com>
2016-07-26 23:54:07 -0700

    Add image presence validation test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,18 +9,5 @@ describe Item do
   it { should belong_to(:section) }
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:section_id) }
-
-  # it { should have_attached_file(:image) }
-  # it { should validate_attachment_presence(:image) }
-
-  # it { should validate_attachment_content_type(:icon).
-  #     allowing('image/png', 'image/gif').
-  #     rejecting('text/plain', 'text/xml') }
-
-  # it { should validate_attachment_size(:image).
-  #               less_than(2.megabytes) }
-  # it { should validate_attachment_size(:icon).
-  #               greater_than(1024) }
-  # it { should validate_attachment_size(:icon).
-  #               in(0..100) }
+  it { should validate_presence_of(:image) }
 end
