e6f8745acbadefa415c2699a6b104c8590cabc47
whatthemandy <amcdorman@gmail.com>
2016-07-27 00:29:51 -0700

    Add tests for presence validations

--- spec/models/outfits_item_spec.rb
+++ spec/models/outfits_item_spec.rb
@@ -3,4 +3,6 @@ require 'spec_helper'
 describe OutfitsItem do
   it { should belong_to(:outfit) }
   it { should belong_to(:item) }
+  it { should validate_presence_of(:outfit_id) }
+  it { should validate_presence_of(:item_id) }
 end
